b9f2cac0d32da4ef9d2b9f022f1d9d6465c5e7ff
Urs Metz <urs.metz@gmx.de>
2014-11-06 22:04:19 +0100

    even one test is enough for full mutation coverage

--- src/test/java/mutation/testing/examples/StringCalcWithArraysTest.java
+++ src/test/java/mutation/testing/examples/StringCalcWithArraysTest.java
@@ -7,14 +7,8 @@ import static org.hamcrest.Matchers.is;
 
 public class StringCalcWithArraysTest {
     @Test
-    public void addSomeNumbers() throws Exception {
-        int result = new StringCalcWithArrays().add("0,0");
-        assertThat(result, is(0));
-    }
-
-    @Test
-    public void addTwoNumbers() throws Exception {
-        int result = new StringCalcWithArrays().add("1,0");
-        assertThat(result, is(1));
+    public void addingOneNumber() throws Exception {
+        int result = new StringCalcWithArrays().add("3");
+        assertThat(result, is(3));
     }
 }
