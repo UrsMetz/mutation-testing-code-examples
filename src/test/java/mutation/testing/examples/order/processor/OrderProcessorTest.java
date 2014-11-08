1ed1b279100cc04a2378f6733b58aa490c2d35e3
Urs Metz <urs.metz@gmx.de>
2014-11-08 14:56:05 +0100

    check billing system call in test
    But the upper casing of article name is not properly tested. As there is
    no mutator in PIT 1.1.1 that catches this the missing test case for
    making the article name upper case is not revealed by PIT.

--- src/test/java/mutation/testing/examples/order/processor/OrderProcessorTest.java
+++ src/test/java/mutation/testing/examples/order/processor/OrderProcessorTest.java
@@ -1,6 +1,7 @@
 package mutation.testing.examples.order.processor;
 
 import mutation.testing.examples.order.processor.article.OrderArticle;
+import mutation.testing.examples.order.processor.billing.BillingSystem;
 import mutation.testing.examples.order.processor.ordernumber.OrderNumberCreator;
 import org.junit.Test;
 
@@ -9,13 +10,24 @@ import static org.hamcrest.Matchers.is;
 
 public class OrderProcessorTest {
     private static final String ORDER_NUMBER = "O-123";
+    public static final String ARTICLE_NAME = "ARTICLE";
+    private final OrderArticle article = new OrderArticle(ARTICLE_NAME);
 
     @Test
     public void processesOrder() throws Exception {
         OrderProcessor orderProcessor = new OrderProcessor(new BillingSystemDummy(), new OrderNumberCreatorStub());
 
-        String returnedOrderNumber = orderProcessor.processOrder(new OrderArticle("ARTICLE"), 1);
+        String returnedOrderNumber = orderProcessor.processOrder(article, 1);
         assertThat(returnedOrderNumber, is(ORDER_NUMBER));
+    }
+
+    @Test
+    public void tellsBillingSystemAboutArticle() throws Exception {
+        BillingSystemMock billingSystem = new BillingSystemMock();
+        OrderProcessor orderProcessor = new OrderProcessor(billingSystem, new OrderNumberCreatorStub());
+
+        orderProcessor.processOrder(article, 1);
+        assertThat(billingSystem.getNameUserInCall(), is(ARTICLE_NAME));
     }
 
     private static class OrderNumberCreatorStub implements OrderNumberCreator {
@@ -25,4 +37,16 @@ public class OrderProcessorTest {
         }
     }
 
+    private class BillingSystemMock implements BillingSystem {
+        private String nameUserInCall;
+
+        @Override
+        public void bill(String name, double price, int quantity) {
+            nameUserInCall = name;
+        }
+
+        public String getNameUserInCall() {
+            return nameUserInCall;
+        }
+    }
 }
