219c16a8d3c635d41a1c5e1009174296f959e15c
Urs Metz <urs.metz@gmx.de>
2014-11-09 17:49:02 +0100

    Adder: Test mit schlechten Testdaten

--- src/test/java/mutation/testing/examples/add/AdderTest.java
+++ src/test/java/mutation/testing/examples/add/AdderTest.java
@@ -2,9 +2,18 @@ package mutation.testing.examples.add;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class AdderTest {
     @Test
     public void adding() throws Exception {
         Adder.add(1, 2);
+    }
+
+    @Test
+    public void addingZeros() throws Exception {
+        int sum = Adder.add(0, 0);
+        assertThat(sum, is(0));
     }
 }
