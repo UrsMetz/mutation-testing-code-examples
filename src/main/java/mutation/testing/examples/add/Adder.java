7da30354cb00bc530d67fbf2e8567d0cf644c4a3
Urs Metz <urs.metz@gmx.de>
2014-11-09 17:33:48 +0100

    add Adder and a completely useless test for it

---
+++ src/main/java/mutation/testing/examples/add/Adder.java
@@ -0,0 +1,7 @@
+package mutation.testing.examples.add;
+
+public class Adder {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/mutation/testing/examples/add/AdderTest.java
@@ -0,0 +1,10 @@
+package mutation.testing.examples.add;
+
+import org.junit.Test;
+
+public class AdderTest {
+    @Test
+    public void adding() throws Exception {
+        Adder.add(1, 2);
+    }
+}
