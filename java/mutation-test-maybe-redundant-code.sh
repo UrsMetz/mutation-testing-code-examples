de16aefa7ca7d83e3306a2efc717cb70d9428cfb
Urs Metz <urs.metz@gmx.de>
2014-11-23 21:10:03 +0100

    extract notify-send part into separate script

--- java/mutation-test-maybe-redundant-code.sh
+++ java/mutation-test-maybe-redundant-code.sh
@@ -1,1 +1,1 @@
-mvn clean test-compile org.pitest:pitest-maven:mutationCoverage -DtargetClasses=mutation.testing.examples.maybe.redundantcode* -DtargetTests=mutation.testing.examples.maybe.redundantcode* && notify-send "mvn fertig"
+mvn clean test-compile org.pitest:pitest-maven:mutationCoverage -DtargetClasses=mutation.testing.examples.maybe.redundantcode* -DtargetTests=mutation.testing.examples.maybe.redundantcode* && ./finished.sh
