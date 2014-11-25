54aaeebdf7c0dfc9740cdfd5a5899ae9216366ea
Urs Metz <urs.metz@gmx.de>
2014-11-25 18:46:55 +0100

    document source of example

--- java/src/main/java/mutation/testing/examples/equivalentmutations/IndexWithMaxValueFinder.java
+++ java/src/main/java/mutation/testing/examples/equivalentmutations/IndexWithMaxValueFinder.java
@@ -1,5 +1,6 @@
 package mutation.testing.examples.equivalentmutations;
 
+// source: https://www.st.cs.uni-saarland.de/edu/testingdebugging10/slides/10-MutationTesting.pdf
 public class IndexWithMaxValueFinder {
     public int findIndexOfMax(int[] numbers) {
         int r = 0;
